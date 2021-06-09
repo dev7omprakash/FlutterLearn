02ffee479ea4fa3ce49e446a07240147ef5f9256
Om prakash <omprakash72810@gmail.com>
2021-06-09 22:48:16 +0530

    New Features added and APP updated

--- scratchandwin/lib/HomePage.dart
+++ scratchandwin/lib/HomePage.dart
@@ -15,12 +15,18 @@ class _HomePageState extends State<HomePage> {
 //TODO:GET THE ARRAY AND VARIABLES
   List<String> itemArray = [];
   int luckyNumber = -1;
+  int count = 0;
+  bool win = false;
+  String message = "YOU HAVE 6 MOVES";
 
 //TODO:INTIALIZE THE ARRAY AND VARIABLES
   @override
   void initState() {
     super.initState();
     setState(() {
+      this.win = false;
+      this.message = "YOU HAVE 6 MOVES";
+      this.count = 0;
       this.itemArray = List<String>.generate(16, (index) => "empty");
       getLuckyNumber();
     });
@@ -49,14 +55,29 @@ class _HomePageState extends State<HomePage> {
   }
 
 //TODO:PLAYGAME METHODS
+
   playGame(int index) {
-    if (luckyNumber == index) {
-      setState(() {
-        itemArray[index] = "lucky";
-      });
+    if (!this.win && this.count < 6) {
+      this.count++;
+      this.message = "YOU HAVE " + (6 - this.count).toString() + " MOVES LEFT";
+      if (luckyNumber == index) {
+        setState(() {
+          this.win = true;
+          this.message = "YOU WON";
+          itemArray[index] = "lucky";
+        });
+      } else {
+        setState(() {
+          itemArray[index] = "unlucky";
+        });
+      }
     } else {
       setState(() {
-        itemArray[index] = "unlucky";
+        if (!this.win && this.count == 6) {
+          this.message = "GAME DRAWN, RESET | SHOW ALL";
+        } else {
+          this.message = "YOU CAN'T PLAY YOU CAN RESET!";
+        }
       });
     }
   }
@@ -64,7 +85,10 @@ class _HomePageState extends State<HomePage> {
 //TODO:RESET GAME METHODS
   resetGame() {
     setState(() {
+      this.win = false;
       this.itemArray = List<String>.filled(16, "empty");
+      this.count = 0;
+      this.message = "YOU HAVE 6 MOVES";
       getLuckyNumber();
     });
   }
@@ -74,6 +98,7 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       this.itemArray = List<String>.filled(16, "unlucky");
       this.itemArray[luckyNumber] = "lucky";
+      this.message = "YOU HACKED THE GAME!";
     });
   }
 
@@ -90,7 +115,7 @@ class _HomePageState extends State<HomePage> {
         children: <Widget>[
           Expanded(
             child: GridView.builder(
-              padding: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(25.0),
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 4,
                 childAspectRatio: 1.0,
@@ -112,8 +137,21 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
+          Center(
+            child: Container(
+              padding: EdgeInsets.all(13.0),
+              child: Text(
+                this.message,
+                style: TextStyle(
+                  color: Colors.blueGrey[800],
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
           Container(
-            margin: EdgeInsets.all(15.0),
+            margin: EdgeInsets.all(8.0),
             child: RaisedButton(
               onPressed: () {
                 this.showAll();
@@ -127,7 +165,7 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           Container(
-            margin: EdgeInsets.all(15.0),
+            margin: EdgeInsets.all(10.0),
             child: RaisedButton(
               onPressed: () {
                 this.resetGame();
